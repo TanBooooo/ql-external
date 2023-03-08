dd00cde7789c5c67837eeffc333deef27b8478fe
TanBooooo <18580902653@163.com>
2023-03-08 22:02:47 +0800

    修改环境变量获取失败问题

--- utils/CommonUtil.py
+++ utils/CommonUtil.py
@@ -61,7 +61,7 @@ def del_txt(file_name):
         log.error(f"{file_name}不存在或不是一个文件")
 
 
-def get_thread_number(size, thread_name='ThreadNumber'):
+def get_thread_number(size, thread_name='THREAD_NUMBER'):
     """获取线程数"""
     thread_number = 5
     value = get_env(thread_name)
@@ -84,7 +84,7 @@ def get_thread_number(size, thread_name='ThreadNumber'):
     return thread_number
 
 
-def get_proxy_api(application=None, proxy_name='Proxy_API'):
+def get_proxy_api(application=None, proxy_name='PROXY_API'):
     """获取代理API"""
     api_url = ''
     if application is not None and application != '':
